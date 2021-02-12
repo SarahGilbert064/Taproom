02a149964126dde69911264ca817ab081643b268
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-02-12 10:43:19 -0800

    create NewBeerForm function with propTypes

---
+++ src/components/NewBeerForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { v4 } from 'uuid';
+import Proptypes from "prop-types";
+
+function NewBeerForm(props) {
+
+  function handleNewBeerFormSubmission(event) {
+    event.preventDefault();
+    props.onNewBeerCreation({
+      name: event.target.name.value,
+      brand: event.target.brand.value,
+      price: event.target.brand.value,
+      alcoholContent: event.target.brand.value,
+      id: v4()
+    });
+  }
+
+  return (
+    <React.Fragment>
+      
+    </React.Fragment>
+  );
+}
+
+NewBeerForm.propTypes = {
+  onNewBeerCreation: Proptypes.func
+};
+
+export default NewTicketForm;
